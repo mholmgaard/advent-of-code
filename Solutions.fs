1f392b6b7259e9e3baf82652710f06ed991d88d1
unknown <mikkel.h.andersen@jp.dk>
2021-12-04 18:15:03 +0100

    Completed third challenge and made some improvements to the code

--- Solutions.fs
+++ Solutions.fs
@@ -1,8 +1,14 @@
 ﻿open System
 open System.IO
 
+// Global helpers
+let read(day: int) = File.ReadAllLines($@"challenges/{day}.txt")
+
+// Solutions
 let december1st =
-    File.ReadAllLines(@"challenges/1.txt") |> Array.map Int32.Parse
+    let toArrayInt(array: string[]) = array |> Array.map Int32.Parse
+
+    toArrayInt(read(1))
     |> Array.pairwise
     |> Array.filter(fun (current, next) -> current < next)
     |> Array.length
@@ -15,15 +21,43 @@ let december2nd =
         | a when a[0].Contains("forward") -> (a[1] |> Int32.Parse, 0) 
         | _                               -> (0, 0)
     let resTuple =
-        File.ReadAllLines(@"challenges/2.txt")
-        |> Array.map(fun s -> matcher s.Split(" "))
+        read(2)
+        |> Array.map(fun s -> s.Split(" "))
+        |> Array.map(fun arr -> matcher arr)
         |> Array.fold(fun (x, y)(z, w) -> (x + z, y + w)) (0, 0)
     fst resTuple * snd resTuple
 
+let december3rd =
+    let mostFrequest(array: int[]) =
+        array
+        |> Array.countBy id
+        |> Array.maxBy snd
+        |> fst
+
+    let toSplitIntArray(array: string[]) = array |> Array.map(fun s -> s.ToCharArray()|> Array.map(fun i -> int(Char.GetNumericValue(i))))
+
+    let array = toSplitIntArray(read(3))
+    let gammaRateBinaryArray = array |> Array.transpose |> Array.map(fun a -> mostFrequest(a))
+
+    let flipBit(bit: int) =
+        match bit with
+        | 0 -> 1
+        | 1 -> 0
+        | _ -> raise(NotImplementedException())
+
+    let epsilonRateBinaryArray = gammaRateBinaryArray |> Array.map(flipBit)
+
+    let flattenArray(array: int[]) = array |> Array.map string |> String.concat ""
+    let binaryStringToInt(binary: string) = Convert.ToInt32(binary, 2)
+
+    binaryStringToInt(flattenArray(gammaRateBinaryArray)) * binaryStringToInt(flattenArray(epsilonRateBinaryArray))
+
+    
 [<EntryPoint>]
 let main args =
     let input = args.[0]
     if (input = "1") then printf "%A" december1st
     else if (input = "2") then printf "%A" december2nd
+    else if (input = "3") then printf "%A" december3rd
     else printf "%A" "NOT_IMPLEMENTED"
     0
