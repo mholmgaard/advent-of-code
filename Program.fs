6346668d45e13d2a11cc23679a803698c83113a9
unknown <mikkel.h.andersen@jp.dk>
2021-12-08 20:06:43 +0100

    Use input in file lookup

--- Program.fs
+++ Program.fs
@@ -1,12 +1,12 @@
 ﻿open System.IO
 
-let read(day: int) = File.ReadAllLines($@"challenges/{day}.txt")
+let read(day: string) = File.ReadAllLines($@"challenges/{day}.txt")
     
 [<EntryPoint>]
 let main args =
     let input = args[0]
-    if      (input = "1") then printf "%A" (Dec01.result(read(1)))
-    else if (input = "2") then printf "%A" (Dec02.result(read(2)))
-    else if (input = "3") then printf "%A" (Dec03.result(read(3)))
+    if      (input = "1") then printf "%A" (Dec01.result(read(input)))
+    else if (input = "2") then printf "%A" (Dec02.result(read(input)))
+    else if (input = "3") then printf "%A" (Dec03.result(read(input)))
     else printf "%A" "NOT_IMPLEMENTED"
     0
